7bea8a2674f3944300cc59404268ef994d2cd98a
Alice Freda <alicefreda@gmail.com>
2020-03-31 16:03:35 -0700

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+    def index
+        @cart = cart
+        render 'index'
+    end
+
+    def add
+        @cart = cart
+        @cart << params[:product]
+        render 'index'
+    end
+end
